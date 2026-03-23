d0278969d99a490921fe5884e29bf0ef16c9628c
NosinkD <ricardruizc@gmail.com>
2026-03-23 08:35:02 +0100

    modified positions to gget ready on road

--- configuration/frames/button.lua
+++ configuration/frames/button.lua
@@ -1,16 +1,12 @@
 local name, ns = ...
 
-function ns.builder.CreateButton(self, labelText, onClick, buttonText)
+function ns.builder.CreateButton(self, labelText, onClick, buttonText, size, anchor, offset)
 	local label = self.optionsPanel:CreateFontString(nil, "ARTWORK", "GameFontNormal")
-	label:SetPoint("TOPLEFT", self.anchor, "BOTTOMLEFT", 0, -16)
-	local font, _, flags = label:GetFont()
-	label:SetFont(tostring(font), 12, flags)
-	label:SetTextColor(1, 1, 1, 1)
-	label:SetText(" " .. (labelText or ""))
+	label:SetPoint("TOPLEFT", anchor or self.anchor, "BOTTOMLEFT", 0, 0)
 
 	local button = CreateFrame("Button", nil, self.optionsPanel, "UIPanelButtonTemplate")
-	button:SetPoint("LEFT", label, "RIGHT", 10, 0)
-	button:SetSize(140, 22)
+	button:SetPoint("LEFT", anchor, "RIGHT", offset or 0, 0)
+	button:SetSize(size.width or 140, size.height or 22)
 	button:SetText(buttonText or labelText or "")
 
 	if type(onClick) == "function" then
